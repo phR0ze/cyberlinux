cf1c46364f5f6176b038e5d5ff864db893d9af89
phR0ze <prc451@gmail.com>
2017-06-15 16:09:56 -0600

    0.0.11: Working on getting basic tests working

--- Rakefile
+++ Rakefile
@@ -1,7 +1,8 @@
 task :default => :build
 
 task :build do
-  system("bundle install")
+  system("bundle install --system")
+  system("./test.rb -p")
 end
 
 # vim: ft=ruby:ts=2:sw=2:sts=2
---
+++ test.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require_relative 'reduce'
+require 'minitest/autorun'
+require 'yaml'
+
+class TestFileInsert < Minitest::Test
+  def setup
+    @file = 'foo'
+    @reduce = Reduce.new
+  end
+
+  # Append, create file
+  def test_multi_value_no_regex_create_file
+
+    # Using mocks
+    mock = Minitest::Mock.new
+    mock.expect(:readlines, 'Lorem ipsum de foo bar')
+    File.stub(:open, mock){|x|
+      puts(x.readlines)
+    }
+    #assert_mock mock
+
+    # Using stubs
+    FileUtils.stub(:touch, 'foo'){|x|
+      File.stub(:exist?, false, 'foo'){|x|
+        FileUtils.touch('foo') if not File.exist?('foo')
+      }
+    }
+  end
+end
+
+# vim: ft=ruby:ts=2:sw=2:sts=2
