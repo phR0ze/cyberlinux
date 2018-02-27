b146e1fb1acd39e1bd8821b7b57670e959d94dbf
phR0ze <prc451@gmail.com>
2018-02-26 18:21:48 -0700

    0.1.247: pkgs: collapsing heavy into desktop and theater

--- layers/desktop/etc/skel/bin/conky_cal.rb
+++ layers/desktop/etc/skel/bin/conky_cal.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #MIT License
-#Copyright (c) 2018 phR0ze
+#Copyright (c) 2017-2018 phR0ze
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -19,19 +19,24 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
+require 'date'
 
-def day_suffix(day)
-  if day.between?(11, 13)
-    "th"
-  else
-    case day % 10
-      when 1; "st"
-      when 2; "nd"
-      when 3; "rd"
-      else    "th"
-    end
-  end
-end
-print(day_suffix(Time.now.day))
+# Get number of spaces to use from the only param passed in
+spaces = ARGV.first.to_i
+
+# Generate array of days with first of the month spacing
+# [' ', '1', '2', '3', ... , '31'] 
+now = Date.today
+prefix = Date.new(now.year, now.month, 1).wday.to_i
+end_of_month = Date.new(now.year, now.month, -1).day
+[*1..end_of_month].unshift(*[* [' '] * prefix]).each_slice(7){|wk|
+  wk.each{|day|
+    print("#{' ' * spaces}")
+    print("${color2}") if day == now.day
+    print("#{day.to_s.rjust(2)}")
+    print("${color1}") if day == now.day
+  }
+  puts
+}
 
 # vim: ft=ruby:ts=2:sw=2:sts=2
